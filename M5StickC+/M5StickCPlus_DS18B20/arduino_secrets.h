a1f09db0f5c7dc73f314848e0fd2c5a31b0410d1
Claus Kühnel <info@ckuehnel.ch>
2023-05-29 14:36:45 +0200

    File Uplaod

---
+++ M5StickC+/M5StickCPlus_DS18B20/arduino_secrets.h
@@ -0,0 +1,7 @@
+char ssid[] = "<Your SSID>";
+char password[] = "<Your Password>";
+
+// Pushover settings
+char pushoversite[] = "api.pushover.net";
+char apitoken[] = "<Your API Token>";
+char userkey [] = "<Your User Key>";
