3728491aeb90a3b1f8b4b2efb0ea614eb4b6e1e9
Claus Kühnel <info@ckuehnel.ch>
2024-05-30 16:26:49 +0200

    File upload

---
+++ ESP32/ESP32_MQTT_Meshtastic/arduino_secrets.h
@@ -0,0 +1,11 @@
+// This file contains credentials to access several services
+
+// WiFi
+#define SECRET_SSID "Sunrise_2.4GHz_8AC2A0"
+#define SECRET_PASS "u2u7fgzv31Ds"
+
+
+// TTN MQTT 
+const char broker[] = "broker.hivemq.com";
+int        port     = 1883;
+const char topic[]  = "msh/#";
